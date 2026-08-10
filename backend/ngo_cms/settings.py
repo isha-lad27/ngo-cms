e2e2580f9ed0cbe1ad7f62442c8dfb00229d95e9
Isha <isha.lad@sdbi.in>
2026-08-10 20:12:47 +0530

    Prepare Django app for Render

--- backend/ngo_cms/settings.py
+++ backend/ngo_cms/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
