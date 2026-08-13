ca4b6685f0c15f2cdf3273cf30b0b0cefa7b26ef
Isha <isha.lad@sdbi.in>
2026-08-13 17:19:52 +0530

    Allow deployed frontend CORS

--- backend/ngo_cms/settings.py
+++ backend/ngo_cms/settings.py
@@ -60,9 +60,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -145,4 +145,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://ngo-cms-frontend.onrender.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ngo-cms-frontend.onrender.com",
 ]
