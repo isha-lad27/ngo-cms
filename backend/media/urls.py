47c2ceeeb6e1af628d7b4ace969394fb1acf6c38
Isha <isha.lad@sdbi.in>
2026-08-17 19:42:08 +0530

    Add media API endpoint

--- backend/media/urls.py
+++ backend/media/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.media_gallery, name="media_gallery"),
+    path("api/", views.media_api, name="media_api"),
 ]
--- backend/media/views.py
+++ backend/media/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import ImageGallery, MediaCoverage
 
 
@@ -14,3 +15,29 @@ def media_gallery(request):
             "media_coverages": media_coverages,
         }
     )
+
+
+def media_api(request):
+    images = ImageGallery.objects.all()
+    media_coverages = MediaCoverage.objects.all()
+
+    image_data = [
+        {
+            "description": image.description,
+            "image": request.build_absolute_uri(image.image.url),
+        }
+        for image in images
+    ]
+
+    coverage_data = [
+        {
+            "title": coverage.title,
+            "url": coverage.url,
+        }
+        for coverage in media_coverages
+    ]
+
+    return JsonResponse({
+        "images": image_data,
+        "media_coverages": coverage_data,
+    })
